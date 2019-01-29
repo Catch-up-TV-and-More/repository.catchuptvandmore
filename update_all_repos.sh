126d6b857ca0fcda9ecf931f14cdc8259bf9f6e7
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2019-01-29 22:11:41 +0100

    Remove auto update of Leai test DRM

--- update_all_repos.sh
+++ update_all_repos.sh
@@ -52,10 +52,6 @@ krypton_release_addon_xml=`cat ./zips/krypton_release/plugin.video.catchuptvandm
 krypton_release_current_version=`extract_addon_version "$krypton_release_addon_xml"`
 echo -e "\t* Krypton release addon version on the repo: $krypton_release_current_version"
 
-leia_drmtest_addon_xml=`cat ./zips/leia_drmtest/plugin.video.catchuptvandmoretestdrmkodi18/addon.xml`
-leia_drmtest_current_version=`extract_addon_version "$leia_drmtest_addon_xml"`
-echo -e "\t* Leia DRM addon version on the repo: $leia_drmtest_current_version"
-
 
 
 
@@ -77,11 +73,6 @@ echo -e "\t* Krypton beta addon last version: $krypton_beta_last_version"
 krypton_release_addon_xml=`wget https://raw.github.com/Catch-up-TV-and-More/plugin.video.catchuptvandmore/master/addon.xml -q -O -`
 krypton_release_last_version=`extract_addon_version "$krypton_release_addon_xml"`
 echo -e "\t* Krypton release addon last version: $krypton_release_last_version"
-
-leia_drmtest_addon_xml=`wget https://raw.github.com/Catch-up-TV-and-More/plugin.video.catchuptvandmoretestdrmkodi18/master/addon.xml -q -O -`
-leia_drmtest_last_version=`extract_addon_version "$leia_drmtest_addon_xml"`
-echo -e "\t* Leia DRM addon last version: $leia_drmtest_last_version"
-
 
 
 
@@ -159,25 +150,6 @@ else
 		--checksum ./addons_xmls/krypton_release/addons.xml.md5 \
 		./repo_addons_src/catchuptvandmore.kodi.krypton.release/ \
 		https://github.com/Catch-up-TV-and-More/plugin.video.catchuptvandmore\#master
-fi
-
-
-
-if [ "$leia_drmtest_current_version" == "$leia_drmtest_last_version" ]
-then
-	echo -e "\t* Last version of Leia DRM test already on the repo"
-else
-	echo -e "\t* New version detected for Leai DRM test"
-	commit_msg="$commit_msg Leai DRM test,"
-	need_to_commit_push=true
-
-	echo -e "\t\t- Start create_repository.py on Leai DRM test repo"
-	python ./create_repository.py \
-		--datadir ./zips/leia_drmtest \
-		--info ./addons_xmls/leia_drmtest/addons.xml \
-		--checksum ./addons_xmls/leia_drmtest/addons.xml.md5 \
-		./repo_addons_src/catchuptvandmore.kodi.leia.drmtest/ \
-		https://github.com/Catch-up-TV-and-More/plugin.video.catchuptvandmoretestdrmkodi18\#master
 fi
 
 
